177406cf63cf762698dec0c16b4b46b9ee158b8b
LG <gl717@outlook.com>
2021-08-11 22:48:14 +0800

    add unit case of display copied text after remove tape

--- src/__tests__/CopyDogeContainer.test.js
+++ src/__tests__/CopyDogeContainer.test.js
@@ -28,4 +28,20 @@ describe("test CopyDogeContainer", () => {
       expect(paragraph).toBeNull();
     })
   })
+
+  it("should display copied text after removing tape", async () => {
+    render(<CopyDogeContainer />);
+
+    const input = screen.getByRole("textbox");
+    userEvent.type(input, "Hello, Doge");
+
+    const image = screen.getByAltText("copydoge");
+    userEvent.click(image);
+
+    const quietImage = await screen.findByAltText("quietdoge");
+    userEvent.click(quietImage);
+
+    const paragraph = await screen.findByText("Hello, Doge");
+    expect(paragraph).toBeInTheDocument();
+  })
 })
